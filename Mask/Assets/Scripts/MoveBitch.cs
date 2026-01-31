665815bb770a7ebe3a3f9ab04ebd6e159e89274e
Ethan Hankinson <esh64887@gmail.com>
2026-01-30 23:49:04 -0400

    PLEASE WORK

---
+++ Mask/Assets/Scripts/MoveBitch.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MoveBitch : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int moveSpeed = 5;
+    Transform transform;
+    void Start()
+    {
+        transform = transform;
+    }
+
+
+    Vector3 getMoveDir()
+    {
+        Vector3 dir = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            dir.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            dir.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            dir.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            dir.x += 1;
+        }
+        return dir;
+    }
+
+    void move()
+    {
+        Vector3 moveDir = getMoveDir();
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+}
