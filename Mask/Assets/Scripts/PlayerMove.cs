e8f1eebc7d7bcd0dfe56c629704dd8926d324756
DanyLaptop <DanyDiab04@gmail.com>
2026-01-31 09:26:16 -0400

    player move script and stage added

---
+++ Mask/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Transform playerTransform;
+    void Start(){
+        playerTransform = transform;
+    }
+
+    Vector3 getMoveDir(){
+        Vector3 dir = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)){
+            dir.y += 1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            dir.y -=1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            dir.x -= 1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            dir.x += 1;
+        }
+
+        return dir;
+    }
+
+    void movePlayer(){
+        Vector3 dir = getMoveDir();
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+
+    }
+
+
+
+
+    void Update(){
+        movePlayer();
+    }
+}
